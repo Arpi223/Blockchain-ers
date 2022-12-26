6c065b38eaf37bd871f3535ede74527cccb4deb0
cheketz <53073865+cheketz@users.noreply.github.com>
2022-12-26 22:37:02 +0100

    Add files via upload

---
+++ Blockchain/Blockchain/SmartContractFunctions.cs
@@ -0,0 +1,45 @@
+
+
+
+class SmartContract{
+
+    Client[] clients;
+    Miner[] miners;
+    string recievedBlock; //blok koji se salje
+
+
+}
+
+// dodavanje klijenata i minera
+
+void sakupljanjeInformacija(){
+
+for(int i = 0; i < 20, i++){
+
+    clients[i] = getKlijent[i];
+
+}
+
+for(int j = 0; j < 20, j++){
+
+    miners[j] = getMineri[i];
+
+}
+
+}
+
+//slanje bloka
+
+void sendingBlocksToMiners (){
+
+    for(int i = 0; i < miners.Length; i++)
+    {
+            //PROVERAVA DA LI MASINA RADI
+        if miners[i].isStarted == false{
+            //SLANJE
+        }
+
+    }
+
+
+}
