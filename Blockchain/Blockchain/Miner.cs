1989133a349b9237198f07d085c85b310c193e47
Zorannn123 <116072732+Zorannn123@users.noreply.github.com>
2022-12-26 22:49:09 +0100

    Kreiranje minera

---
+++ Blockchain/Blockchain/Miner.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockChain
+{
+    internal class Miner
+    {
+        private string Id;
+        private string CurrentState;
+        private bool started = false;
+
+        public string GetId() {
+            return Id;
+        }
+
+        public string GetCurrentState() {
+            return CurrentState;
+        }
+
+        public bool isStarted() {
+            if (started = true)
+                return false;
+            else
+                return true;
+        }
+
+    }
+
+
+
+
+}
