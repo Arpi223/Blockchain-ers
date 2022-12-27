42886c835c9217dc81d746a6d7d82efbc6846fd8
Barsi Árpád <barsiarpi223@gmail.com>
2022-12-27 11:25:11 +0100

    wcf changes
    add changes to block chain

---
+++ Client/Program.cs
@@ -0,0 +1,38 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string address = "net.tcp://localhost:4000/ISmartContractClient";
+            NetTcpBinding binding = new NetTcpBinding();
+
+            ChannelFactory<ISmartContractClient> channel = new ChannelFactory<ISmartContractClient>(binding, address);
+
+            ISmartContractClient proxy = channel.CreateChannel();
+
+            int counter = 0;
+            while (true) { 
+            Random rand = new Random();
+            Common.Client client = new Common.Client(counter, rand.Next(1000));
+
+            proxy.Calculate(client);
+
+            counter++;
+            Thread.Sleep(10000);
+                }
+
+
+            Console.ReadKey();
+        }
+    }
+}
